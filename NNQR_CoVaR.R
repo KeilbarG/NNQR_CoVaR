decea242bd25e6b257946ed0cba563269aa19558
KeilbarG <georgkeilbar@gmail.com>
2018-10-08 17:15:14 +0200

    Add files via upload

--- NNQR_CoVaR.R
+++ NNQR_CoVaR.R
@@ -2,15 +2,7 @@
 # clear all variables
 rm(list = ls(all = TRUE))
 graphics.off()
-setwd("C:/Users/Georg_PC/Documents/Thesis/Final_20180626/Code")
-
-
-# install and load packages
-libraries = c("quantreg","qrnn","NeuralNetTools","quantmod","h2o","igraph")
-lapply(libraries, function(x) if (!(x %in% installed.packages())) {
-  install.packages(x)
-})
-lapply(libraries, library, quietly = TRUE, character.only = TRUE)
+setwd("~/Desktop/Thesis/20180626_Final/Code")
 
 
 # read data
@@ -22,11 +14,11 @@ CoVaR = as.matrix(read.csv(file = "CoVaR.csv"))
 
 
 # plot returns, VaR and CoVaR for each bank
-par(mfrow = c(4, 2), mar = c(2.5, 2, 3.5, 1))
+par(mfrow = c(3, 3), mar = c(2.5, 2, 3.5, 1))
 
 for (j in 1 : ncol(x0)) {
   plot(x0[, j] ~ date, type = "p",
-       ylim = c(min(x0[, j], CoVaR[, j]) * 1.1, max(x0[, j]) * 1.1),
+       ylim = c(-0.6,0.35) , #c(min(x0[, j], CoVaR[, j]) * 1.1, max(x0[, j]) * 1.1)
        ylab = "", pch=".", cex = 3, main = colnames(x0)[j], cex.main = 2)
   lines(VaR[, j] ~ date, col = "blue", lwd = 2)
   lines(CoVaR[, j] ~ date, col = "red", lwd = 2)
